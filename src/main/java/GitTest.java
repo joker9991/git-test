3a8ae43a3b84896f2b09d45887355881b3a9d08b
zhangsan <a792700497@163.com>
2021-11-16 21:27:13 +0800

    hot-fix commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("git test!");
         System.out.println("git test!");
         System.out.println("git  test!");
+        System.out.println("hot-fix text");
     }
 }
